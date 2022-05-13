943babea49f0b3e931051bf4cb2309c3bb50655a
LHJ <2014542916@qq.com>
2022-05-13 19:58:01 +0800

    重构1

--- mall-admin/src/main/resources/db.migration/1.0.1/V1__creat_product_details.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `product_details` (
-                                   `product_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '属性id',
-                                   `product_company` varchar(30) DEFAULT NULL COMMENT '生产公司',
-                                   `product_date` date NOT NULL COMMENT '日期',
-                                   PRIMARY KEY (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1003 DEFAULT CHARSET=utf8mb4 COMMENT='商品详情'
