bbd32896580728ea1b9668362519aeed8931d306
lihaojie <2014542916@qq.com>
2022-07-04 20:48:39 +0800

    generator ok

---
+++ mall-admin/src/main/java/com/niu/mall/admin/controller/ProductController.java
@@ -0,0 +1,68 @@
+package com.niu.mall.admin.controller;
+
+
+import com.niu.mall.admin.domain.Product;
+import com.niu.mall.admin.service.impl.ProductServiceImpl;
+import com.niu.mall.common.api.Result;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>
+ * 商品基本信息 前端控制器
+ * </p>
+ *
+ * @author lhj
+ * @since 2022-07-03
+ */
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private ProductServiceImpl productService;
+
+    /**
+     *  创建商品
+     *   形参：ProductParamDto
+     */
+    @ApiOperation("创建商品控制类")
+    @PostMapping(value = "/product")
+    public Result create(@ApiParam("商品参数,内封装ProductAttribute,ProductBasic,productDetails") @RequestBody Product product) {
+
+        return productService.createProduct(product);
+    }
+
+    /**
+     * 更新商品
+     * @Param：id
+     * */
+    @ApiOperation("更新商品控制类")
+    @PutMapping(value = "/product")
+    public Result update( @RequestBody Product product) {
+        return productService.update(product);
+    }
+
+    /**
+     * @description: 删除方法
+     * @param: id
+     * @return:  void
+     * @author Administrator
+     * @date: 2022/5/30 15:56
+     */
+    @DeleteMapping("/product/{id}")
+    public String delete(@PathVariable Integer id){
+
+        return null;
+    }
+    @ApiOperation("查询商品控制类")
+    @GetMapping(value = "/product/{id}")
+    public Result getByProductId(@PathVariable long id) {
+        return  productService.getByProductId(id);
+    }
+
+}
+
---
+++ mall-admin/src/main/java/com/niu/mall/admin/service/impl/ProductServiceImpl.java
@@ -0,0 +1,66 @@
+package com.niu.mall.admin.service.impl;
+
+import com.niu.mall.admin.domain.Product;
+import com.niu.mall.admin.dao.ProductDao;
+import com.niu.mall.admin.dto.ProductParamDto;
+import com.niu.mall.admin.service.IProductService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.niu.mall.common.api.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 商品基本信息 服务实现类
+ * </p>
+ *
+ * @author lhj
+ * @since 2022-07-03
+ */
+@Service
+public class ProductServiceImpl extends ServiceImpl<ProductDao, Product> implements IProductService {
+    //@Resource
+    @Autowired
+    private ProductDao productDao;
+
+    @Override
+    public Result createProduct(Product product) {
+
+        product.setProductId(null);
+        int count =productDao.createProduct(product);
+
+        if (count > 0) {
+            return Result.success(product.getProductId(),"商品创建成功");
+        }
+        return Result.failed("fail","商品创建失败");
+    }
+
+    @Override
+    public Result update(Product product) {
+        int count=productDao.update(product);
+        if (count > 0) {
+            return Result.success("success","更新成功");
+        }
+        return Result.failed("fail","更新失败");
+    }
+
+    @Override
+    public Result getByProductId(Long id) {
+        Product product =new Product();
+        productDao.findById(id);
+        /*if (product.getProductName()==null) {
+            System.out.println("空指针异常");
+            return Result.failed("fail","查询失败");
+        }*/
+        System.out.println(product.getProductName()+"22323441111");
+        return Result.success(product,"查询成功");
+    }
+
+    @Override
+    public List<ProductParamDto> list(ProductParamDto productParamDto, Integer pageSize, Integer pageNum) {
+        // List<ProductBasic> list = productParamDto.getProductBasic();
+        return null;
+    }
+}
