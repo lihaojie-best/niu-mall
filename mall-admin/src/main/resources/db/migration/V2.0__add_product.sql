73591510dfcf381477150d7de9e7b39936bf35fc
lihaojie <2014542916@qq.com>
2022-08-31 21:40:24 +0800

    改用MP代码生成器,OK

--- mall-admin/src/main/resources/db/migration/V2.0__add_product.sql
+++
@@ -1,7 +0,0 @@
-/*INSERT INTO product( `product_id`,`product_name`, `product_colour`,`product_company`, `product_type`,
-                       `product_price`,`product_sales_volume`,`product_stock`)
-VALUES (1003,'iphoneXS','白色','Apple','手机',3999,0,2000),
-       (1004,'iphoneX','白色','Apple','手机',2999,0,1000),
-       (1005,'iphoneXS','黑色','Apple','手机',3999,0,1000),
-        (1006,'iphoneX','黑色','Apple','手机',2999,0,1050);
-*/
