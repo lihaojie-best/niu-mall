9bb1c7902f6657c2d64eb4a68ac256f68c3d5236
lihaojie <2014542916@qq.com>
2023-06-02 07:37:56 +0800

    spring security 完成

--- mall-admin/src/main/java/com/niu/mall/config/MallSecurityConfig.java
+++ mall-admin/src/main/java/com/niu/mall/config/MallSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.niu.mall.config;
 
 
+import com.niu.mall.po.UmsResourcePo;
 import com.niu.mall.service.UmsAdminService;
 import com.niu.mall.service.UmsResourceService;
 import com.niu.mall.security.component.DynamicSecurityService;
@@ -8,12 +9,14 @@ import com.niu.mall.security.config.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import javax.annotation.Resource;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -39,13 +42,14 @@ public class MallSecurityConfig extends SecurityConfig {
         return username -> adminService.loadUserByUsername(username);
     }
 
+
+    @DependsOn("dataSource")
     @Bean
     public DynamicSecurityService dynamicSecurityService() {
         return () -> {
             Map<String, ConfigAttribute> map = new ConcurrentHashMap<>();
-            //获取全部资源  下面这行代码有问题 注释掉这行代码就可以启动了 现在项目启动不了！！！
-            //List<UmsResourcePo> list = resourceService.listAll();
-            //list.forEach(resourcePo -> map.put(resourcePo.getUrl(), new org.springframework.security.access.SecurityConfig(resourcePo.getId() + ":" + resourcePo.getName())));
+            List<UmsResourcePo> list = resourceService.listAll();
+            list.forEach(resourcePo -> map.put(resourcePo.getUrl(), new org.springframework.security.access.SecurityConfig(resourcePo.getId() + ":" + resourcePo.getName())));
             return map;
         };
     }
